9ada577bec28a1b49bef85dacaae535764135be3
bayupw <bayupw@gmail.com>
2022-05-11 03:54:11 +1200

    first commit

--- main.tf
+++ main.tf
@@ -97,3 +97,19 @@ module "gcp_prod_spoke_1" {
   instance_size = var.gcp_instance_size
   ha_gw         = var.ha_gw
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Multi-Cloud Transit Peering
+# ---------------------------------------------------------------------------------------------------------------------
+# module "transit-peering" {
+#   source  = "terraform-aviatrix-modules/mc-transit-peering/aviatrix"
+#   version = "1.0.3"
+
+#   transit_gateways = [
+#     module.aws_transit_1.transit_gateway.gw_name,
+#     module.azure_transit_1.transit_gateway.gw_name,
+#     module.gcp_transit_1.transit_gateway.gw_name,
+#   ]
+
+#   excluded_cidrs = ["0.0.0.0/0"]
+# }
